657a978ec480cb3342ead63a617478fbe3ac9df4
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-01 19:42:04 +0900

    Create asm_space.h

---
+++ header file/asm_space.h
@@ -0,0 +1,29 @@
+int asm_space(Dctab* dp) {
+    int len;
+
+    if (!Operand) {
+        fprintf(stderr, "%s ---> Operand is not defined ...\n", LBUF);
+        exit(10);
+    }
+
+    if (dp->type == DC_RESV) {
+        if ((!isdigit(Operand[0]) && Operand[strlen(Operand) - 1] != 'h') ||
+            (len = str2int(Operand)) == INV_VAL) {
+            fprintf(stderr, "%s ---> Operand number '%s' is not valid ...\n", LBUF, Operand);
+            exit(11);
+        }
+        len *= dp->unit;
+        OBJC[0] = 0;
+
+        return len;
+    }
+
+    if (dp->unit == 1 && Operand[0] == '\'') {
+        len = cal_dc_oprnd_string(Operand, OBJC);
+    }
+    else {
+        len = cal_dc_oprnd(Operand, dp->unit, OBJC);
+    }
+
+    return len;
+}
