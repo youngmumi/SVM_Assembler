ccf7d1f2b405d1e09119defea7c26f70e3ee91fb
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-01 19:42:44 +0900

    Create cal_expr.h

---
+++ header file/cal_expr.h
@@ -0,0 +1,23 @@
+int cal_one_expr(char* expr) {
+	int expv, v1, v2, sign;
+	char* pp = NULL, * mp = NULL, * v2p = NULL;
+
+	if (((sign = issign(*expr)) != 0)) expv++;
+	else sign = 1;
+
+	if (pp = strchr(expr, '+')) *pp = 0;
+	else if (mp = strchr(expr, '-')) *mp = 0;
+
+	if (pp) v2p = pp + 1;
+	else if (mp) v2p = mp + 1;
+
+	v1 = sign * cnv_one_term(expr);
+
+	if (v2p) v2 = cnv_one_term(v2p);
+
+	if (pp) expv = v1 + v2;
+	else if (mp) expv = v1 - v2;
+	else expv = v1;
+
+	return(expv);
+}
