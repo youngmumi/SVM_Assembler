6025d2e71c6ab858c451294669d4c21bc5144a64
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-01 19:45:21 +0900

    Create symtab.h

---
+++ header file/symtab.h
@@ -0,0 +1,32 @@
+#define MAX_SYM	100
+#define MAX_SLEN (8+1)
+#define INV_VAL (-0x7fffffff)
+#define RMD_OPR 0x7fffffff
+
+typedef struct symtab {
+	char symbol[MAX_SLEN];
+	int value;
+} Symtab;
+
+int SYMCNT = 1;
+Symtab SYMTAB[MAX_SYM] = { "RND", RND_OPR, };
+
+int ins_SYMTAB(int value, char* sym) {
+	if (see_SYMTAB(sym) != INV_VAL)
+		return 0;
+	strcpy(SYMTAB[SYMCNT].symbol, sym);
+	SYMTAB[SYMCNT].value = value;
+	SYMCNT++;
+	return 1;
+}
+
+int see_SYMTAB(char* sym) {
+	int i;
+
+	for (i = 0; i < SYMCNT; i++) {
+		if (!strcmp(SYMTAB[i].symbol, sym))
+			return(SYMTAB[i].value);
+	}
+
+	return INV_VAL;
+}
