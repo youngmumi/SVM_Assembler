c9fc852f225948cbecd4dd73328036ee66916f19
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-01 19:44:15 +0900

    Create put_list.h

---
+++ header file/put_list.h
@@ -0,0 +1,14 @@
+void put_list_head() {
+	printf(" LNO	Loation		Label	Opcode	Operand		Object Code\n");
+	printf(" ---	-------		-----	------	-------		-----------\n");
+}
+
+void put_list() {
+	int i;
+	printf("%4s %.7X %-8s %-8s %-16s", LNO, LOC, LABEL, OPcode, OPerand), fflush(stdout);
+
+	for (i = 0; i < OBJC[0]; i++)
+		printf("%.2X", OBJC[i + 1]);
+	printf("\n");
+	OBJC[0] = 0;
+}
